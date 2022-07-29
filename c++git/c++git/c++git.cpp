f8695e45797ab4b7e7d13d3fba76d1e6b991be8b
sqdqdwd <109736999+sqdqdwd@users.noreply.github.com>
2022-07-29 21:03:21 +0200

    Update c++git.cpp

--- c++git/c++git/c++git.cpp
+++ c++git/c++git/c++git.cpp
@@ -2,25 +2,6 @@
 using namespace std;
 int main()
 {
-   int num3 , num2 , result    ;
-    char op;
-    cout << "heloo enter  number1 \n";
-    cin >> num3;
-    cout << "heloo enter  number2 \n";
-    cin >> num2;
-    cout << "heloo enter  math operation  \n";
-    cin >> op ;
-    if (op ==  '+' )
-    {
-        result = num3 + num2;
-    }
-    else if (op == '-')
-    {
-        result = num3 - num2;
-    }
-    else
-    {
-        cout << "cant do that operation /n";
-    }
-    cout << "the reult = "<< result;
+
+    cout << "the reult = ";
 }
