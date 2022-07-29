21d0f2b808887af3df56210bf25c7fc36897f7d8
karim1211 <110127777+karim1211@users.noreply.github.com>
2022-07-29 21:07:00 +0200

    Update c++git.cpp

--- c++git/c++git/c++git.cpp
+++ c++git/c++git/c++git.cpp
@@ -2,6 +2,25 @@
 using namespace std;
 int main()
 {
-
-    cout << "the reult = ";
+int num1 , num2 , result    ;
+    char op;
+    cout << "heloo enter  number1 \n";
+    cin >> num1;
+    cout << "heloo enter  number2 \n";
+    cin >> num2;
+    cout << "heloo enter  math operation  \n";
+    cin >> op ;
+    if (op ==  '+' )
+    {
+        result = num1 + num2;
+    }
+    else if (op == '-')
+    {
+        result = num1 - num2;
+    }
+    else
+    {
+        cout << "cant do that operation /n";
+    }
+    cout << "the reult = "<< result;
 }
