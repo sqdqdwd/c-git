fe7489a4f1a9a92d3adf5a9cddaaadc16df3e9c9
sqdqdwd <mohaned.menem@gmail.com>
2022-08-03 19:41:47 +0200

    first commit

--- c++git/c++git.cpp
+++ c++git/c++git.cpp
@@ -3,9 +3,34 @@
 using namespace std;
 int main()
 {
-    int x;
-    cout << "Hello World sir \n";
-    cin >> x;
-    cout << "your number is " << x;
-    cout << "يا نووب يا كريم";
+    int num1, num2, result;
+    char op;
+    cout << "heloo enter  number1 \n";
+    cin >> num1;
+    cout << "heloo enter  number2 \n";
+    cin >> num2;
+    cout << "hloo enter  math operation  \n";
+    cin >> op;
+    if (op == '+')
+    {
+        result = num1 + num2;
+    }
+    else if (op == '-')
+    {
+        result = num1 - num2;
+    }
+    else if (op == '*')
+    {
+        result = num1 * num2;
+    }
+    else if (op == '/')
+    {
+        result = num1 / num2;
+    }
+    else
+    {
+        cout << "cant do that operation /n";
+    }
+    cout << "the reult = "<< result ;
+    
 }
